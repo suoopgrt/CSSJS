7d5c27af41ef226c4275d1b01cfe8878faca71c0
James Im <jamesim@Jamess-iMac.local>
2021-03-15 13:56:22 -0700

    adding

--- CSS/JSR/js/homescript.js
+++ CSS/JSR/js/homescript.js
@@ -1,12 +1,17 @@
 (function(global){
 	var dc = {};
 	var httpHtml = "snippets/http-snippet.html";
-	// var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+//  var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
 	var allCategoriesUrl = "data/categories.json"
+//	var menuItemsUrl ="https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+	var canvasUrl = "data/canvas-items.json";
 
 
 	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 	var categoryHtml = "snippets/category-snippet.html";
+
+	var canvastitlehtml = "snippets/canvas-title.html";
+	var canvasitemHtml = "snippets/canvas-item.html";
 
 	var insertHtml = function(selector, html){
 		var targetElem = document.querySelector(selector);
@@ -39,6 +44,52 @@
 		$ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
 	};
 
+	dc.loadCanvasItems = function(categoryShort){
+		showLoading("#HTTPDes");
+		$ajaxUtils.sendGetRequest(
+				canvasUrl, buildAndShowCanvasItemsHTML
+			);
+	}
+
+	function buildAndShowCanvasItemsHTML(categoryCanvasItems){
+		$ajaxUtils.sendGetRequest(
+				canvastitlehtml, function(canvastitlehtml){
+					$ajaxUtils.sendGetRequest(canvasitemHtml, function(canvasItem){
+						switchCanvasToActive();
+						var canvasItemViewHtml = 
+						buildCanvasItemsViewhtml(categoryCanvasItems,canvastitlehtml, canvasItem);
+						insertHtml("HTTPDes", canvasItemViewHtml);
+					}, false);
+				}, false );
+	}
+	// Using categories data and snippets html
+	// build categories view HTML to be inserted into page
+	function buildCategoriesViewHtml(categories,
+	                                 categoriesTitleHtml,
+	                                 categoryHtml) {
+
+	  var finalHtml = categoriesTitleHtml;
+	  finalHtml += "<section class='row'>";
+
+	  // Loop over categories
+	  for (var i = 0; i < categories.length; i++) {
+	    // Insert category values
+	    var html = categoryHtml;
+	    var name = "" + categories[i].name;
+	    var short_name = categories[i].short_name;
+	    html =
+	      insertProperty(html, "name", name);
+	    html =
+	      insertProperty(html,
+	                     "short_name",
+	                     short_name);
+	    finalHtml += html;
+	  }
+
+	  finalHtml += "</section>";
+	  return finalHtml;
+	}
+	
 	function buildAndShowCategoriesHTML(categories){
 		console.log("buildAndShowCategoriesHTML: "+categories);
 		$ajaxUtils.sendGetRequest(
@@ -50,6 +101,20 @@
 				}, false);
 		}, false);
 	}
+	// Remove the class 'active' from home and switch to Menu button
+	var switchMenuToActive = function () {
+	  // Remove 'active' from home button
+	  var classes = document.querySelector("#navHomeButton").className;
+	  classes = classes.replace(new RegExp("active", "g"), "");
+	  document.querySelector("#navHomeButton").className = classes;
+
+	  // Add 'active' to menu button if not already there
+	  classes = document.querySelector("#navMenuButton").className;
+	  if (classes.indexOf("active") == -1) {
+	    classes += " active";
+	    document.querySelector("#navMenuButton").className = classes;
+	  }
+	};
 
 	function buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml){
 		var finalHtml = categoriesTitleHtml;
